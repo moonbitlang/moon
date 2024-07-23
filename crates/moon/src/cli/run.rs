a1216a304b623bd1591c422064b3bf18346479bc
Li Junchen <lijunchen95@outlook.com>
2024-07-23 10:34:40 +0800

    update license header

--- crates/moon/src/cli/run.rs
+++ crates/moon/src/cli/run.rs
@@ -1,4 +1,20 @@
-use std::path::PathBuf;
+// moon: The build system and package manager for MoonBit.
+// Copyright (C) 2024 International Digital Economy Academy
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// For inquiries, you can contact us via e-mail at jichuruanjian@idea.edu.cn.
 
 use anyhow::{bail, Context};
 use moonbuild::dry_run;
@@ -16,6 +32,7 @@ use moonutil::dirs::PackageDirs;
 use moonutil::mooncakes::sync::AutoSyncFlags;
 use moonutil::mooncakes::RegistryConfig;
 use n2::trace;
+use std::path::PathBuf;
 
 use super::{BuildFlags, UniversalFlags};
 
