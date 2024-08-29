152d93df401b79d2bb0f6957b7e88b225bbc2c25
Young-Flash <dongyang@apache.org>
2024-08-29 10:23:58 +0800

    internal: don't use reserved word in js driver

--- crates/moonbuild/template/js_driver.js
+++ crates/moonbuild/template/js_driver.js
@@ -1,14 +1,14 @@
 const { execute } = require("origin_js_path");
 
-const package = "";
-const test_params = [];
+const packageName = "";
+const testParams = [];
 
-for (param of test_params) {
+for (param of testParams) {
     try {
         execute(param[0], parseInt(param[1]));
     } catch (e) {
         console.log("----- BEGIN MOON TEST RESULT -----")
-        console.log(`{"package": "${package}", "filename": "${param[0]}", "index": "${param[1]}", "test_name": "${param[1]}", "message": "${e.stack.toString().split('\n').join('\\n')}"}`);
+        console.log(`{"package": "${packageName}", "filename": "${param[0]}", "index": "${param[1]}", "test_name": "${param[1]}", "message": "${e.stack.toString().split('\n').join('\\n')}"}`);
         console.log("----- END MOON TEST RESULT -----")
     }
 }
