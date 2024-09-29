7d75880347ea88adf0e9b61d912748550dbca4e6
Young-Flash <dongyang@apache.org>
2024-09-29 14:53:25 +0800

    fix js test args for debug (#351)

--- crates/moonbuild/template/js_driver.js
+++ crates/moonbuild/template/js_driver.js
@@ -1,17 +1,22 @@
 const { moonbit_test_driver_internal_execute } = require("origin_js_path");
 
-let testArgs;
+let packageName = "";
+let testParams = [];
+
 try {
-  testArgs = JSON.parse(process.argv[2]);
+  // check if there is command line argument
+  if (process.argv.length > 2) {
+    const testArgs = JSON.parse(process.argv[2]);
+    packageName = testArgs.package;
+    testParams = testArgs.file_and_index.flatMap(([file, range]) =>
+      Array.from({ length: range.end - range.start }, (_, i) => [file, (range.start + i).toString()])
+    );
+  }
 } catch (error) {
-  console.error("failed to parse args:", error.message);
+  console.error("failed to parse args: ", error.message);
   process.exit(1);
 }
 
-const packageName = testArgs.package;
-const testParams = testArgs.file_and_index.flatMap(([file, range]) => 
-  Array.from({length: range.end - range.start}, (_, i) => [file, (range.start + i).toString()])
-);
 
 for (param of testParams) {
     try {
