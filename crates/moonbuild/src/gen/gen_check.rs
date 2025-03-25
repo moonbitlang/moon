db44417f7493a15b0240c480d4f86847a97f53d7
Li Junchen <lijunchen95@outlook.com>
2025-03-25 14:50:11 +0800

    can_dirty_on_output flag was set incorrectly

--- crates/moonbuild/src/gen/gen_check.rs
+++ crates/moonbuild/src/gen/gen_check.rs
@@ -443,7 +443,7 @@ pub fn gen_check_command(
         "check: {}",
         get_desc_name(&item.package_full_name, &item.mi_out)
     ));
-    build.can_dirty_on_output = false;
+    build.can_dirty_on_output = true;
     build
 }
 
