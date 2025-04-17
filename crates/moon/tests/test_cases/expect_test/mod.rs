2d2b56f66b18829fa74ca2e7229e2ae05452bfb6
Rynco Maekawa <i@rynco.me>
2025-04-17 10:32:59 +0800

    migrate: backend_config, expect_test, inline_test, mbti, moon_bundle, moon_commands

---
+++ crates/moon/tests/test_cases/expect_test/mod.rs
@@ -0,0 +1,62 @@
+use super::*;
+
+#[cfg(unix)]
+#[test]
+fn test_expect_test() -> anyhow::Result<()> {
+    let tmp_dir_path = TestDir::new("expect_test/expect_test");
+
+    let s = snapbox::cmd::Command::new(moon_bin())
+        .current_dir(&tmp_dir_path)
+        .args(["test", "--update", "--no-parallelize"])
+        .assert()
+        .success()
+        .get_output()
+        .stdout
+        .to_owned();
+    let out = std::str::from_utf8(&s).unwrap().to_string();
+
+    assert!(out.contains("Auto updating expect tests and retesting ..."));
+    assert!(out.contains("Total tests: 30, passed: 30, failed: 0."));
+    let updated =
+        std::fs::read_to_string(tmp_dir_path.as_ref().join("lib").join("hello.mbt")).unwrap();
+    assert!(updated.contains(r#"["a", "b", "c"]"#));
+
+    let s = snapbox::cmd::Command::new(moon_bin())
+        .current_dir(&tmp_dir_path)
+        .args(["test", "--update", "--no-parallelize"])
+        .assert()
+        .success()
+        .get_output()
+        .stdout
+        .to_owned();
+
+    let out = std::str::from_utf8(&s).unwrap().to_string();
+    assert!(out.contains("Total tests: 30, passed: 30, failed: 0."));
+    let out = std::fs::read_to_string(tmp_dir_path.as_ref().join("lib").join("hello_wbtest.mbt"))
+        .unwrap();
+    assert!(out.contains(r#"inspect!(notbuf, content="haha")"#));
+    Ok(())
+}
+
+#[test]
+fn test_only_update_expect() {
+    let tmp_dir_path = TestDir::new("expect_test/only_update_expect");
+
+    let _ = snapbox::cmd::Command::new(moon_bin())
+        .current_dir(&tmp_dir_path)
+        .args([
+            "test",
+            "-p",
+            "username/hello/lib",
+            "-f",
+            "hello.mbt",
+            "-i",
+            "0",
+            "--update",
+        ])
+        .assert()
+        .failure()
+        .get_output()
+        .stdout
+        .to_owned();
+}
--- crates/moon/tests/test_cases/moon_commands/mod.rs
+++ crates/moon/tests/test_cases/moon_commands/mod.rs
@@ -15,3 +15,99 @@ fn test_moon_cmd() {
         "#]],
     );
 }
+
+#[test]
+fn test_moon_help() {
+    let dir = TestDir::new_empty();
+    check(
+        get_stdout(&dir, ["help"]).replace("moon.exe", "moon"),
+        expect![[r#"
+            The build system and package manager for MoonBit.
+
+            Usage: moon [OPTIONS] <COMMAND>
+
+            Commands:
+              new                    Create a new MoonBit module
+              build                  Build the current package
+              check                  Check the current package, but don't build object files
+              run                    Run a main package
+              test                   Test the current package
+              clean                  Remove the target directory
+              fmt                    Format source code
+              doc                    Generate documentation
+              info                   Generate public interface (`.mbti`) files for all packages in the module
+              bench                  Run benchmarks in the current package
+              add                    Add a dependency
+              remove                 Remove a dependency
+              install                Install dependencies
+              tree                   Display the dependency tree
+              login                  Log in to your account
+              register               Register an account at mooncakes.io
+              publish                Publish the current module
+              package                Package the current module
+              update                 Update the package registry index
+              coverage               Code coverage utilities
+              generate-build-matrix  Generate build matrix for benchmarking (legacy feature)
+              upgrade                Upgrade toolchains
+              shell-completion       Generate shell completion for bash/elvish/fish/pwsh/zsh to stdout
+              version                Print version information and exit
+              help                   Print this message or the help of the given subcommand(s)
+
+            Options:
+              -h, --help  Print help
+
+            Common Options:
+              -C, --directory <SOURCE_DIR>   The source code directory. Defaults to the current directory
+                  --target-dir <TARGET_DIR>  The target directory. Defaults to `source_dir/target`
+              -q, --quiet                    Suppress output
+              -v, --verbose                  Increase verbosity
+                  --trace                    Trace the execution of the program
+                  --dry-run                  Do not actually run the command
+                  --build-graph              Generate build graph
+        "#]],
+    );
+}
+
+#[test]
+#[ignore]
+#[cfg(unix)]
+fn test_bench4() {
+    let dir = TestDir::new_empty();
+    get_stdout(&dir, ["generate-build-matrix", "-n", "4", "-o", "bench4"]);
+    check(
+        get_stdout(
+            &dir,
+            [
+                "run",
+                "--source-dir",
+                "./bench4",
+                "--target-dir",
+                "./bench4/target",
+                "main",
+            ],
+        ),
+        expect![[r#"
+            ok
+        "#]],
+    );
+
+    get_stdout(
+        &dir,
+        [
+            "run",
+            "--source-dir",
+            "./bench4",
+            "--target-dir",
+            "./bench4/target",
+            "--trace",
+            "main",
+        ],
+    );
+
+    let trace_file = dunce::canonicalize(dir.join("./trace.json")).unwrap();
+    let t = std::fs::read_to_string(trace_file).unwrap();
+    assert!(t.contains("moonbit::build::read"));
+    assert!(t.contains(r#""name":"work.run""#));
+    assert!(t.contains(r#""name":"run""#));
+    assert!(t.contains(r#""name":"main""#));
+}
