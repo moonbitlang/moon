cfa656c591d2a33b7a54b209e5e4864022f1a386
Young-Flash <dongyang@apache.org>
2025-04-28 14:55:37 +0800

    internal: skip coverage for panic & intrinsics pkg

--- crates/moonbuild/src/gen/mod.rs
+++ crates/moonbuild/src/gen/mod.rs
@@ -27,6 +27,8 @@ pub mod util;
 // WORKAROUND for do not test coverage on coverage library itself
 const MOON_CORE_COVERAGE_LIB: &str = "moonbitlang/core/coverage";
 const MOON_CORE_BUILTIN_LIB: &str = "moonbitlang/core/builtin";
+const MOON_CORE_PANIC_LIB: &str = "moonbitlang/core/panic";
+const MOON_CORE_INTRINSICS_LIB: &str = "moonbitlang/core/intrinsics";
 
 #[test]
 fn test_start_with() {
@@ -35,7 +37,7 @@ fn test_start_with() {
     assert!(MOON_CORE_BUILTIN_LIB.starts_with(MOONBITLANG_CORE));
 }
 
-static SKIP_COVERAGE_LIBS: &[&str] = &[];
+static SKIP_COVERAGE_LIBS: &[&str] = &[MOON_CORE_PANIC_LIB, MOON_CORE_INTRINSICS_LIB];
 static SELF_COVERAGE_LIBS: &[&str] = &[MOON_CORE_BUILTIN_LIB, MOON_CORE_COVERAGE_LIB];
 
 fn is_skip_coverage_lib(name: &str) -> bool {
