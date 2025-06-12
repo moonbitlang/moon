5f971faead7772a645ef5cbd7f94aa494d0faa48
Rynco Maekawa <i@rynco.me>
2025-06-12 14:03:12 +0800

    refactor: Better build target specification

--- crates/moonbuild-rupes-recta/src/model.rs
+++ crates/moonbuild-rupes-recta/src/model.rs
@@ -70,6 +70,12 @@ use std::{collections::HashMap, path::PathBuf};
 
 use arcstr::ArcStr;
 use moonutil::mooncakes::ModuleId;
+use petgraph::prelude::DiGraphMap;
+
+slotmap::new_key_type! {
+    /// An unique identifier pointing to a package currently discovered from imported modules.
+    pub struct PackageId;
+}
 
 /// Represents the target of this build routine.
 #[derive(Clone, Debug, Copy, PartialEq, Eq)]
@@ -89,18 +95,35 @@ pub enum TargetTask {
     MakeExecutable,
 }
 
-#[derive(Clone, Debug, Copy, PartialEq, Eq)]
+#[derive(Clone, Debug, Copy, PartialEq, Eq, Hash, Ord, PartialOrd)]
 pub enum TargetKind {
     Source,
     WhiteboxTest,
     BlackboxTest,
+
+    // TODO: do we really need to specify inline tests as a separate target kind,
+    // or should it be just `Source` with tests enabled?
+    InlineTest,
+    /// This is the subpackage designed originally for breaking cycles in
+    /// `moonbitlang/core`. It's expected to be used sparingly.
+    SubPackage,
 }
 
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub struct TargetSpecifier {
+/// Represents a single compile target that may be separately checked, built,
+/// linked, etc.
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Copy, PartialOrd, Ord)]
+pub struct BuildTarget {
+    pub package: PackageId,
     pub kind: TargetKind,
-    pub module: ModuleId,
-    pub package: ArcStr,
+}
+
+impl PackageId {
+    pub fn build_target(self, kind: TargetKind) -> BuildTarget {
+        BuildTarget {
+            package: self,
+            kind,
+        }
+    }
 }
 
 /// Represents a single target, like ordinary source, whitebox test files, etc.
@@ -110,7 +133,7 @@ pub struct Target {
     files: Vec<PathBuf>,
     c_stubs: Vec<PathBuf>,
     /// The dependent package names
-    deps: Vec<TargetSpecifier>,
+    deps: Vec<BuildTarget>,
 }
 
 #[derive(Clone, Debug, Default)]
@@ -118,10 +141,4 @@ pub struct PackageTargets {
     source_target: Target,
     whitebox_test: Option<Target>,
     blackbox_test: Option<Target>,
-}
-
-/// The list of all targets
-#[derive(Clone, Debug, Default)]
-pub struct AllTargets {
-    map: HashMap<ArcStr, PackageTargets>,
 }
