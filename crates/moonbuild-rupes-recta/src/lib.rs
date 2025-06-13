87c51234b10f45aee3dbb5df92ed3aa3b957a206
Rynco Maekawa <i@rynco.me>
2025-06-13 11:42:09 +0800

    docs: Update documentation for Rupes Recta build process and design principles

--- crates/moonbuild-rupes-recta/src/lib.rs
+++ crates/moonbuild-rupes-recta/src/lib.rs
@@ -1,3 +1,46 @@
+/*!
+    Rupes Recta is the new build graph generator for MoonBuild.
+
+    The overall design of Rupes Recta is very similar to that of Rust's build
+    system, [Cargo](https://docs.rs/cargo/latest/cargo/), although it is
+    designed independently without referencing much from it.
+
+    The modules within this project aim to be implemented under the
+    [*Single-responsibility
+    Principle*](https://en.wikipedia.org/wiki/Single-responsibility_principle).
+    Each module should perform one and only one step within the build program,
+    and different steps of building should be done separately instead of being
+    coupled together within one step.
+
+    The rough steps (with Rupes Recta) of building a MoonBit program are:
+
+    1. Read the `mooncakes.io` registry and resolve the *module* dependency
+        graph ([`mooncake::resolver`]).
+    2. Download the required dependency to local cache folders
+        ([`mooncake::pkg::install`]).
+    3. Discover packages within modules ([`crate::discover`]). This is different
+        from many package managers -- the package distribution unit ("module")
+        is different from the compile unit ("package").
+    4. Resolve the *package* dependency graph ([`crate::solve`]).
+    5. Get the list of top-level build actions from user input.
+    6. From this list of build actions, resolve the whole abstract build graph
+        that represents the list of actions to be executed
+        ([`crate::abs_build_graph`]).
+    7. Lower the build graph to a concrete one acceptable by [`n2`] (which is an
+        in-process `ninja` equivalent) ([`crate::build_graph_lower`]).
+    8. Execute the build graph using `n2`.
+
+    Additional information about the build process, project layout, special
+    cases, and random quirks of build systems can be found in the repository's
+    documentation, at `/docs/dev/reference`.
+
+    ## Alternative design
+
+    An alternative is proposed, but not implemented, with replacing `n2` with
+    a hand-rolled, in-process executor that directly works on the abstract build
+    graph instead of requiring to lower every command beforehand.
+*/
+
 mod cond_comp;
 pub mod discover;
 pub mod model;
