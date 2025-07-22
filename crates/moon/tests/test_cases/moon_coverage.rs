24317836dee189a9b03216dd77dbabc933aac86e
Rynco Maekawa <i@rynco.me>
2025-07-22 17:56:54 +0800

    Promote tests

--- crates/moon/tests/test_cases/moon_coverage.rs
+++ crates/moon/tests/test_cases/moon_coverage.rs
@@ -88,7 +88,7 @@ fn test_moon_coverage_analyze_dry_run() {
             moon generate-test-driver --output-driver ./target/wasm-gc/debug/test/lib/__generated_driver_for_whitebox_test.mbt --output-metadata ./target/wasm-gc/debug/test/lib/__whitebox_test_info.json ./lib/hello_wbtest.mbt --target wasm-gc --pkg-name username/hello/lib --enable-coverage --driver-kind whitebox
             moonc build-package ./lib/hello.mbt ./lib/hello_wbtest.mbt ./target/wasm-gc/debug/test/lib/__generated_driver_for_whitebox_test.mbt -o ./target/wasm-gc/debug/test/lib/lib.whitebox_test.core -pkg username/hello/lib -is-main -pkg-sources username/hello/lib:./lib -target wasm-gc -g -O0 -source-map -enable-coverage -whitebox-test -no-mi -test-mode
             moonc link-core ./target/wasm-gc/debug/test/lib/lib.whitebox_test.core -main username/hello/lib -o ./target/wasm-gc/debug/test/lib/lib.whitebox_test.wasm -test-mode -pkg-config-path ./lib/moon.pkg.json -pkg-sources username/hello/lib:./lib -exported_functions moonbit_test_driver_internal_execute,moonbit_test_driver_finish -target wasm-gc -g -O0 -source-map
-            (cd $ROOT && moon_cove_report -f=caret)
+            (cd $ROOT && moon_cove_report -f=simp_caret)
         "#]],
     );
 }
