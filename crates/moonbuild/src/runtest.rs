c49eaeb122d5870aada07b4221770ce4c04d7245
Wen Yuxiang <hackwaly@qq.com>
2025-07-29 14:23:15 +0800

    add --enable-source-maps cli option to node

--- crates/moonbuild/src/runtest.rs
+++ crates/moonbuild/src/runtest.rs
@@ -101,7 +101,7 @@ pub async fn run_js(
         node,
         path,
         target_dir,
-        &[],                                             // args before path
+        &["--enable-source-maps".to_string()],            // args before path
         &[serde_json_lenient::to_string(args).unwrap()], // args after path
         file_test_info_map,
         verbose,
