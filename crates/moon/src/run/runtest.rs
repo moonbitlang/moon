f2337e8ad62db98f73d466fbc51ffc786b355ab0
Rynco Maekawa <i@rynco.me>
2025-08-08 16:57:54 +0800

    feat(rr): Initial test running

--- crates/moon/src/run/runtest.rs
+++ crates/moon/src/run/runtest.rs
@@ -18,21 +18,89 @@
 
 //! Run tests and interpret the results
 
-use std::collections::HashMap;
+use std::{collections::HashMap, path::Path};
 
 use anyhow::Context;
-use moonbuild::entry::TestArgs;
-use moonbuild_rupes_recta::model::{BuildPlanNode, TargetAction};
-use moonutil::common::MooncGenTestInfo;
+use moonbuild::{
+    benchmark::BATCHBENCH,
+    entry::TestArgs,
+    expect::{ERROR, EXPECT_FAILED, RUNTIME_ERROR, SNAPSHOT_TESTING},
+    runtest::TestStatistics,
+    section_capture::SectionCapture,
+};
+use moonbuild_rupes_recta::model::{BuildPlanNode, BuildTarget, TargetAction};
+use moonutil::common::{
+    MooncGenTestInfo, MOON_COVERAGE_DELIMITER_BEGIN, MOON_COVERAGE_DELIMITER_END,
+    MOON_TEST_DELIMITER_BEGIN, MOON_TEST_DELIMITER_END,
+};
+use tokio::runtime::Runtime;
 
 use crate::{rr_build::CompileOutput, run::default_rt};
 
+enum TestResultKind {
+    Passed,
+    ExpectTestFailed,
+    SnapshotTestFailed,
+    RuntimeError,
+    ExpectPanic,
+    Failed,
+}
+
+struct TestCaseResult {
+    kind: TestResultKind,
+    _raw: TestStatistics, // will use later for test promotion
+}
+
+impl TestCaseResult {
+    pub fn passed(&self) -> bool {
+        matches!(self.kind, TestResultKind::Passed)
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct TestResult {
+    pub total: usize,
+    pub passed: usize,
+}
+
+impl TestResult {
+    pub fn merge(&mut self, other: TestResult) {
+        self.total += other.total;
+        self.passed += other.passed;
+    }
+
+    pub fn passed(&self) -> bool {
+        self.total == self.passed
+    }
+}
+
 /// Run the tests compiled in this session. Returns `true` if all tests passed,
 /// `false` if any test failed. TODO: actual test info display
-pub fn run_tests(ret: &CompileOutput) -> anyhow::Result<bool> {
+pub fn run_tests(compile_output: &CompileOutput, target_dir: &Path) -> anyhow::Result<TestResult> {
     // Gathering artifacts
+    let results = gather_tests(compile_output);
+
+    let rt = default_rt().context("Failed to create runtime")?;
+    let mut stats = TestResult::default();
+    for r in results {
+        let res = run_one_test_executable(compile_output, &rt, target_dir, &r)?;
+        stats.merge(res);
+    }
+
+    Ok(stats)
+}
+
+struct TestExecutableToRun<'a> {
+    target: BuildTarget,
+    executable: &'a Path,
+    meta: &'a Path,
+}
+
+/// Gather tests executables from the compilation output.
+fn gather_tests(ret: &CompileOutput) -> Vec<TestExecutableToRun<'_>> {
     let mut pending = HashMap::new();
-    let mut results = vec![]; // (executable, metadata)
+    let mut results = vec![];
+
     for artifacts in &ret.artifacts {
         let corresponding = match artifacts.node.action {
             TargetAction::MakeExecutable => TargetAction::GenerateTestInfo,
@@ -58,54 +126,192 @@ pub fn run_tests(ret: &CompileOutput) -> anyhow::Result<bool> {
                 continue;
             }
         };
-        results.push((artifacts.node.target, exec, meta));
+        results.push(TestExecutableToRun {
+            target: artifacts.node.target,
+            executable: exec,
+            meta,
+        });
     }
 
-    let rt = default_rt().context("Failed to create runtime")?;
-    for (node, executable, metadata_path) in results {
-        let metadata =
-            std::fs::File::open(metadata_path).context("Failed to open test metadata")?;
-        let metadata = serde_json_lenient::from_reader::<_, MooncGenTestInfo>(metadata)
-            .with_context(|| {
-                format!(
-                    "Failed to parse test metadata at {}",
-                    metadata_path.display()
-                )
-            })?;
+    results
+}
 
-        let pkgname = ret
-            .resolve_output
-            .pkg_dirs
-            .get_package(node.package)
-            .fqn
-            .to_string();
+fn run_one_test_executable(
+    ret: &CompileOutput,
+    rt: &Runtime, // FIXME: parallel execution
+    target_dir: &Path,
+    test: &TestExecutableToRun,
+) -> Result<TestResult, anyhow::Error> {
+    let meta = std::fs::File::open(test.meta).context("Failed to open test metadata")?;
+    let meta: MooncGenTestInfo = serde_json_lenient::from_reader(meta)
+        .with_context(|| format!("Failed to parse test metadata at {}", test.meta.display()))?;
 
-        // Convert MooncGenTestInfo to TestArgs format
-        let mut test_args = TestArgs {
-            package: pkgname,
-            file_and_index: vec![],
-        };
+    let pkgname = ret
+        .resolve_output
+        .pkg_dirs
+        .get_package(test.target.package)
+        .fqn
+        .to_string();
+    let mut test_args = TestArgs {
+        package: pkgname,
+        file_and_index: vec![],
+    };
 
-        // TODO: add file filtering
-        for lists in [
-            &metadata.no_args_tests,
-            &metadata.with_args_tests,
-            &metadata.with_bench_args_tests,
-        ] {
-            for (filename, test_infos) in lists {
-                if !test_infos.is_empty() {
-                    let max_index = test_infos.iter().map(|t| t.index).max().unwrap_or(0);
-                    test_args
-                        .file_and_index
-                        .push((filename.to_string(), 0..(max_index + 1)));
-                }
+    for lists in [
+        &meta.no_args_tests,
+        &meta.with_args_tests,
+        &meta.with_bench_args_tests,
+    ] {
+        for (filename, test_infos) in lists {
+            if !test_infos.is_empty() {
+                let max_index = test_infos.iter().map(|t| t.index).max().unwrap_or(0);
+                test_args
+                    .file_and_index
+                    .push((filename.to_string(), 0..(max_index + 1)));
             }
         }
-
-        let cmd = crate::run::command_for(ret.target_backend, executable, Some(&test_args));
-        rt.block_on(crate::run::run(&mut [], false, cmd))
-            .context("Failed to run test")?; // TODO
     }
 
-    Ok(true)
+    let cmd = crate::run::command_for(ret.target_backend, test.executable, Some(&test_args));
+    let mut cov_cap = mk_coverage_capture();
+    let mut test_cap = make_test_capture();
+
+    rt.block_on(crate::run::run(
+        &mut [&mut cov_cap, &mut test_cap],
+        false,
+        cmd,
+    ))
+    .context("Failed to run test")?;
+
+    handle_finished_coverage(target_dir, cov_cap)?;
+    let stats = get_test_statistics(&meta, test_cap)?;
+
+    // TODO: update snapshots and expect tests
+
+    let total_count = stats.len();
+    let passed_count = stats.iter().filter(|x| x.passed()).count();
+
+    Ok(TestResult {
+        total: total_count,
+        passed: passed_count,
+    })
+}
+
+fn mk_coverage_capture() -> SectionCapture<'static> {
+    SectionCapture::new(
+        MOON_COVERAGE_DELIMITER_BEGIN,
+        MOON_COVERAGE_DELIMITER_END,
+        true,
+    )
+}
+
+fn make_test_capture() -> SectionCapture<'static> {
+    SectionCapture::new(MOON_TEST_DELIMITER_BEGIN, MOON_TEST_DELIMITER_END, false)
+}
+
+fn handle_finished_coverage(target_dir: &Path, cap: SectionCapture) -> anyhow::Result<()> {
+    if let Some(coverage_output) = cap.finish() {
+        let time = chrono::Local::now().timestamp_micros();
+        let rnd = rand::random::<u32>();
+
+        // Note: maybe we should choose another filename?
+        let filename = target_dir.join(format!("moonbit_coverage_{time}_{rnd:08x}.txt"));
+
+        std::fs::write(&filename, coverage_output).context(format!(
+            "failed to write coverage result to {}",
+            filename.to_string_lossy()
+        ))?;
+    }
+    Ok(())
+}
+
+fn get_test_statistics(
+    meta: &MooncGenTestInfo,
+    cap: SectionCapture,
+) -> anyhow::Result<Vec<TestCaseResult>> {
+    let Some(s) = cap.finish() else {
+        return Ok(vec![]);
+    };
+
+    // Create a map to repopulate test names
+    // map<filename, map<index, test_case_name>>
+    let mut test_name_map: HashMap<&str, HashMap<u32, Option<&str>>> = HashMap::new();
+    for test_list in [
+        &meta.no_args_tests,
+        &meta.with_args_tests,
+        &meta.with_bench_args_tests,
+    ] {
+        for (file, tests) in test_list {
+            let file_map = test_name_map.entry(file.as_str()).or_default();
+            for t in tests {
+                file_map.insert(t.index, t.name.as_deref());
+            }
+        }
+    }
+
+    // Actual handling of each test case result
+    let mut res = vec![];
+    for line in s.lines() {
+        if line.is_empty() {
+            continue;
+        }
+
+        let stat: TestStatistics = serde_json_lenient::from_str(line)
+            .with_context(|| format!("Failed to parse test summary: {line}"))?;
+
+        // Repopulate name.
+        // The test name in stat may be different from that in source code,
+        // due to how cases like panics are handled, causing later handling to
+        // deviate from what we expect. Here, we fetch the name from the
+        // metadata to avoid the problem.
+        let name = test_name_map
+            .get(stat.filename.as_str())
+            .and_then(|v| v.get(&stat.index.parse::<u32>().ok()?).cloned())
+            .flatten()
+            .unwrap_or(&stat.test_name);
+
+        let result_kind = parse_one_test_result(&stat, name)?;
+        res.push(TestCaseResult {
+            kind: result_kind,
+            _raw: stat,
+        });
+    }
+
+    Ok(res)
+}
+
+fn parse_one_test_result(
+    result: &TestStatistics,
+    test_name: &str,
+) -> anyhow::Result<TestResultKind> {
+    use TestResultKind::*;
+
+    let res = if test_name.starts_with("panic") {
+        // For whatever reason, any message in the result is viewed as the code
+        // successfully panicked.
+        // FIXME: random prefix matching is bad, should use a proper attribute or something in code
+        if result.message.is_empty() {
+            ExpectPanic
+        } else {
+            Passed
+        }
+    } else if result.message.starts_with(EXPECT_FAILED) {
+        ExpectTestFailed
+    } else if result.message.starts_with(SNAPSHOT_TESTING) {
+        // FIXME: file access HERE?!
+        if moonbuild::expect::snapshot_eq(&result.message)
+            .with_context(|| format!("Failed to read snapshot for {}", result.test_name))?
+        {
+            Passed
+        } else {
+            SnapshotTestFailed
+        }
+    } else if result.message.starts_with(RUNTIME_ERROR) || result.message.starts_with(ERROR) {
+        RuntimeError
+    } else if result.message.starts_with(BATCHBENCH) || result.message.is_empty() {
+        Passed
+    } else {
+        Failed
+    };
+    Ok(res)
 }
