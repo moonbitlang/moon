ecafdf652f6323725d9b018cd05b9c0c6067c3d8
Rynco Maekawa <i@rynco.me>
2025-09-25 10:56:09 +0800

    test: Add tests to ensure release mode in bench

--- crates/moon/tests/test_cases/moon_bench/mod.rs
+++ crates/moon/tests/test_cases/moon_bench/mod.rs
@@ -5,3 +5,44 @@ fn test_bench_driver_build() {
     let dir = TestDir::new("moon_bench");
     check(get_stderr(&dir, ["bench", "--build-only"]), expect![""]);
 }
+
+#[test]
+fn test_bench_uses_release_mode_by_default() {
+    let dir = TestDir::new("moon_bench");
+
+    // Release by default
+    let dry_run = get_stdout(&dir, ["bench", "--dry-run"]);
+    println!("bench + dry-run output:\n{}", dry_run);
+    assert!(
+        dry_run.contains("moonc"),
+        "Ensure dry run is executed correctly: dry_run.contains(\"moonc\")"
+    );
+    assert!(
+        !dry_run.contains("-O0"),
+        "Ensure release mode is used by default: !dry_run.contains(\"-O0\")"
+    );
+
+    // Explicit release
+    let dry_run = get_stdout(&dir, ["bench", "--dry-run", "--release"]);
+    println!("bench + dry_run + release output:\n{}", dry_run);
+    assert!(
+        dry_run.contains("moonc"),
+        "Ensure dry run is executed correctly: dry_run.contains(\"moonc\")"
+    );
+    assert!(
+        !dry_run.contains("-O0"),
+        "Ensure release mode is used when --release is passed: !dry_run.contains(\"-O0\")"
+    );
+
+    // Explicit debug mode. Unsure why someone would do this for a bench, but ok.
+    let dry_run = get_stdout(&dir, ["bench", "--dry-run", "--debug"]);
+    println!("bench + dry-run + debug output:\n{}", dry_run);
+    assert!(
+        dry_run.contains("moonc"),
+        "Ensure dry run is executed correctly: dry_run.contains(\"moonc\")"
+    );
+    assert!(
+        dry_run.contains("-O0"),
+        "Ensure debug mode is used when --debug is passed: dry_run.contains(\"-O0\")"
+    );
+}
