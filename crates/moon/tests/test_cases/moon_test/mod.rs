66855284eaf8c741f07903f350b7f79bc385f683
Rynco Maekawa <i@rynco.me>
2025-09-26 10:42:52 +0800

    test: Add test for test cfg

--- crates/moon/tests/test_cases/moon_test/mod.rs
+++ crates/moon/tests/test_cases/moon_test/mod.rs
@@ -1,3 +1,5 @@
+mod with_cfg;
+
 use super::*;
 
 #[test]
---
+++ crates/moon/tests/test_cases/moon_test/with_cfg.rs
@@ -0,0 +1,25 @@
+use expect_test::expect;
+
+use crate::{get_stdout, util::check, TestDir};
+
+#[test]
+fn test_moon_test_with_cfg() {
+    let dir = TestDir::new("moon_test/with_cfg");
+    check(
+        get_stdout(&dir, ["test", "--target=wasm"]),
+        expect![[r#"
+            I am always executed
+            I only execute on wasm
+            Total tests: 2, passed: 2, failed: 0.
+        "#]],
+    );
+
+    check(
+        get_stdout(&dir, ["test", "--target=js"]),
+        expect![[r#"
+            I am always executed
+            I only execute on not wasm
+            Total tests: 2, passed: 2, failed: 0.
+        "#]],
+    );
+}
