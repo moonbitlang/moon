b21b5524518c16c86bf41c25c1ab938e47e14dad
Rynco Maekawa <i@rynco.me>
2025-10-24 10:51:44 +0800

    fix(rr): Inherit stdout/stderr, prevent child buffering in `moon run`

--- crates/moon/src/run/child.rs
+++ crates/moon/src/run/child.rs
@@ -26,31 +26,46 @@ use tokio::process::Command;
 
 /// Run a command under the governing of `moon run`.
 ///
-/// `stdout` and `stderr` is always piped, to prevent the subprocess changing
-/// the property of file descriptors (`node` is notorious for this, see
-/// [moon#852](https://github.com/moonbitlang/moon/issues/852)).
+/// If `capture` is set, the output will be captured and can be processed (like
+/// `moon test`). Otherwise, the output will be directly inherited from the
+/// parent process (like `moon run`).
+///
+/// `stdout` and `stderr` is always piped if `capture` is true, to prevent the
+/// subprocess changing the property of file descriptors (`node` is notorious
+/// for this, see [moon#852](https://github.com/moonbitlang/moon/issues/852)).
 ///
 /// `captures` uses a list of [`SectionCapture`] to capture part of the `stdout`
 /// output since the running process might not have any other method to interact
 /// with the host `moon` process.
 pub async fn run<'a>(
     captures: &mut [&mut SectionCapture<'a>],
-    stdin: bool,
+    capture: bool,
     mut cmd: Command,
 ) -> anyhow::Result<ExitStatus> {
-    if stdin {
-        cmd.stdin(Stdio::inherit());
-    } else {
+    if capture {
+        // If we want to capture some/all of the output, we want to set piped
+        // to both streams to prevent `node` and friends changing fd blocking
+        // status
         cmd.stdin(Stdio::null());
+        cmd.stdout(Stdio::piped());
+        cmd.stderr(Stdio::piped());
+    } else {
+        assert!(
+            captures.is_empty(),
+            "Can't have section captures when not capturing stdout"
+        );
+        // We aren't captureing, so YOLO
+        cmd.stdin(Stdio::inherit());
+        cmd.stdout(Stdio::inherit());
+        cmd.stderr(Stdio::inherit());
     }
-    cmd.stdout(Stdio::piped());
-    cmd.stderr(Stdio::piped()); // to prevent `node` and friends changing fd blocking status
     cmd.kill_on_drop(true); // to prevent zombie processes;
 
     let mut child = cmd
         .spawn()
         .with_context(|| format!("Failed to spawn command {:?}", cmd))?;
 
+    // Task only exists when not capturing
     let stderr_pipe_task = child.stderr.take().map(|mut stderr| {
         tokio::spawn(async move {
             let mut proc_stderr = tokio::io::stderr();
@@ -62,17 +77,19 @@ pub async fn run<'a>(
 
     // Since we cannot have scoped async tasks here, and we borrow the capture
     // sections, we'll handle stdout in this main task
-    let child_stdout = child
-        .stdout
-        .take()
-        .expect("Child process should have stdout piped");
-    {
-        let mut buf_stdout = tokio::io::BufReader::new(child_stdout);
+    if capture {
+        let child_stdout = child
+            .stdout
+            .take()
+            .expect("Child process should have stdout piped");
+
         if !captures.is_empty() {
+            let buf_stdout = tokio::io::BufReader::new(child_stdout);
             handle_stdout_async(buf_stdout, captures).await?;
         } else {
-            let mut stdout = tokio::io::stdout();
-            tokio::io::copy_buf(&mut buf_stdout, &mut stdout).await?;
+            let mut child_stdout = child_stdout;
+            let mut proc_stdout = tokio::io::stdout();
+            tokio::io::copy(&mut child_stdout, &mut proc_stdout).await?;
         }
     }
 
