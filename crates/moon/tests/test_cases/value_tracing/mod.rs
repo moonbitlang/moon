4025b894c1b2616820afe387a1c42945cae50e60
Yu Zhang <yu.zhang.yz862@yale.edu>
2025-11-14 10:39:21 +0800

    test: use graph comparison and separate single file test

--- crates/moon/tests/test_cases/value_tracing/mod.rs
+++ crates/moon/tests/test_cases/value_tracing/mod.rs
@@ -59,25 +59,20 @@ fn test_tracing_value_for_test_block() {
 #[test]
 fn test_tracing_value_for_main_func() {
     let dir = TestDir::new("tracing_value.in");
-
+    let run_graph = dir.join("run_graph.jsonl");
     // main.mbt in package
-    check(
-        get_stdout(
-            &dir,
-            [
-                "run",
-                "./main/main.mbt",
-                "--enable-value-tracing",
-                "--dry-run",
-            ],
-        ),
-        expect![[r#"
-            moonc build-package ./lib/hello.mbt -o ./target/wasm-gc/release/build/lib/lib.core -pkg moon_new/lib -std-path $MOON_HOME/lib/core/target/wasm-gc/release/bundle -pkg-sources moon_new/lib:./lib -target wasm-gc -workspace-path .
-            moonc build-package ./main/main.mbt -o ./target/wasm-gc/release/build/main/main.core -pkg moon_new/main -is-main -std-path $MOON_HOME/lib/core/target/wasm-gc/release/bundle -i ./target/wasm-gc/release/build/lib/lib.mi:lib -pkg-sources moon_new/main:./main -target wasm-gc -enable-value-tracing -workspace-path .
-            moonc link-core $MOON_HOME/lib/core/target/wasm-gc/release/bundle/abort/abort.core $MOON_HOME/lib/core/target/wasm-gc/release/bundle/core.core ./target/wasm-gc/release/build/lib/lib.core ./target/wasm-gc/release/build/main/main.core -main moon_new/main -o ./target/wasm-gc/release/build/main/main.wasm -pkg-config-path ./main/moon.pkg.json -pkg-sources moon_new/lib:./lib -pkg-sources moon_new/main:./main -pkg-sources moonbitlang/core:$MOON_HOME/lib/core -target wasm-gc
-            moonrun ./target/wasm-gc/release/build/main/main.wasm
-        "#]],
+    snap_dry_run_graph(
+        &dir,
+        [
+            "run",
+            "./main/main.mbt",
+            "--enable-value-tracing",
+            "--dry-run",
+        ],
+        &run_graph,
     );
+    compare_graphs(&run_graph, expect_file!["run_graph.jsonl.snap"]);
+
     let content = get_stdout(&dir, ["run", "./main/main.mbt", "--enable-value-tracing"])
         .split("######MOONBIT_VALUE_TRACING_START######")
         .filter(|l| !(l.contains("__generated_driver_for")))
@@ -111,18 +106,22 @@ fn test_tracing_value_for_main_func() {
             3
         "#]],
     );
+}
 
+#[test]
+fn test_tracing_value_for_single_file() {
     // single file
+    let dir = TestDir::new("tracing_value.in");
     check(
         get_stdout(
             &dir,
             ["run", "./main.mbt", "--enable-value-tracing", "--dry-run"],
         ),
         expect![[r#"
-            moonc build-package $ROOT/main.mbt -o $ROOT/target/main.core -std-path $MOON_HOME/lib/core/target/wasm-gc/release/bundle -is-main -pkg moon/run/single -g -O0 -source-map -target wasm-gc -enable-value-tracing
-            moonc link-core $MOON_HOME/lib/core/target/wasm-gc/release/bundle/abort/abort.core $MOON_HOME/lib/core/target/wasm-gc/release/bundle/core.core $ROOT/target/main.core -o $ROOT/target/main.wasm -pkg-sources moon/run/single:$ROOT -pkg-sources moonbitlang/core:$MOON_HOME/lib/core -g -O0 -source-map -target wasm-gc
-            moonrun $ROOT/target/main.wasm
-        "#]],
+          moonc build-package $ROOT/main.mbt -o $ROOT/target/main.core -std-path $MOON_HOME/lib/core/target/wasm-gc/release/bundle -is-main -pkg moon/run/single -g -O0 -source-map -target wasm-gc -enable-value-tracing
+          moonc link-core $MOON_HOME/lib/core/target/wasm-gc/release/bundle/abort/abort.core $MOON_HOME/lib/core/target/wasm-gc/release/bundle/core.core $ROOT/target/main.core -o $ROOT/target/main.wasm -pkg-sources moon/run/single:$ROOT -pkg-sources moonbitlang/core:$MOON_HOME/lib/core -g -O0 -source-map -target wasm-gc
+          moonrun $ROOT/target/main.wasm
+      "#]],
     );
     let content = get_stdout(&dir, ["run", "./main.mbt", "--enable-value-tracing"])
         .split("######MOONBIT_VALUE_TRACING_START######")
@@ -134,26 +133,26 @@ fn test_tracing_value_for_main_func() {
         expect![[r#"
 
 
-            {"name": "a", "line": 2, "start_column": 7, "end_column": 8, "filepath": "moon/run/single/main.mbt", "value": "$placeholder"}
-            ######MOONBIT_VALUE_TRACING_CONTENT_START######
-            1
-            ######MOONBIT_VALUE_TRACING_CONTENT_END######
-            ######MOONBIT_VALUE_TRACING_END######
+          {"name": "a", "line": 2, "start_column": 7, "end_column": 8, "filepath": "moon/run/single/main.mbt", "value": "$placeholder"}
+          ######MOONBIT_VALUE_TRACING_CONTENT_START######
+          1
+          ######MOONBIT_VALUE_TRACING_CONTENT_END######
+          ######MOONBIT_VALUE_TRACING_END######
 
 
-            {"name": "b", "line": 3, "start_column": 7, "end_column": 8, "filepath": "moon/run/single/main.mbt", "value": "$placeholder"}
-            ######MOONBIT_VALUE_TRACING_CONTENT_START######
-            2
-            ######MOONBIT_VALUE_TRACING_CONTENT_END######
-            ######MOONBIT_VALUE_TRACING_END######
+          {"name": "b", "line": 3, "start_column": 7, "end_column": 8, "filepath": "moon/run/single/main.mbt", "value": "$placeholder"}
+          ######MOONBIT_VALUE_TRACING_CONTENT_START######
+          2
+          ######MOONBIT_VALUE_TRACING_CONTENT_END######
+          ######MOONBIT_VALUE_TRACING_END######
 
 
-            {"name": "c", "line": 4, "start_column": 7, "end_column": 8, "filepath": "moon/run/single/main.mbt", "value": "$placeholder"}
-            ######MOONBIT_VALUE_TRACING_CONTENT_START######
-            3
-            ######MOONBIT_VALUE_TRACING_CONTENT_END######
-            ######MOONBIT_VALUE_TRACING_END######
-            3
-        "#]],
+          {"name": "c", "line": 4, "start_column": 7, "end_column": 8, "filepath": "moon/run/single/main.mbt", "value": "$placeholder"}
+          ######MOONBIT_VALUE_TRACING_CONTENT_START######
+          3
+          ######MOONBIT_VALUE_TRACING_CONTENT_END######
+          ######MOONBIT_VALUE_TRACING_END######
+          3
+      "#]],
     );
 }
