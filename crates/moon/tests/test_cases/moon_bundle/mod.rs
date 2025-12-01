bb3191f007328ea3cbd63661746a95a0e889d291
Rynco Maekawa <i@rynco.me>
2025-12-01 17:31:40 +0800

    test: Promote tests

--- crates/moon/tests/test_cases/moon_bundle/mod.rs
+++ crates/moon/tests/test_cases/moon_bundle/mod.rs
@@ -6,11 +6,11 @@ fn test_moon_bundle() {
     check(
         get_stdout(&dir, ["bundle", "--dry-run", "--sort-input"]),
         expect![[r#"
-            moonc build-package -w -a -alert -all -o ./target/wasm-gc/release/bundle/prelude/prelude.core -pkg moonbitlang/core/prelude -pkg-sources moonbitlang/core/prelude:./prelude -target wasm-gc -workspace-path .
-            moonc build-package ./Orphan/lib.mbt -w -a -alert -all -o ./target/wasm-gc/release/bundle/Orphan/Orphan.core -pkg moonbitlang/core/Orphan -pkg-sources moonbitlang/core/Orphan:./Orphan -target wasm-gc -workspace-path .
-            moonc build-package ./A/lib.mbt -w -a -alert -all -o ./target/wasm-gc/release/bundle/A/A.core -pkg moonbitlang/core/A -pkg-sources moonbitlang/core/A:./A -target wasm-gc -workspace-path .
-            moonc build-package ./C/lib.mbt -w -a -alert -all -o ./target/wasm-gc/release/bundle/C/C.core -pkg moonbitlang/core/C -i ./target/wasm-gc/release/bundle/A/A.mi:A -pkg-sources moonbitlang/core/C:./C -target wasm-gc -workspace-path .
-            moonc build-package ./B/lib.mbt -w -a -alert -all -o ./target/wasm-gc/release/bundle/B/B.core -pkg moonbitlang/core/B -i ./target/wasm-gc/release/bundle/A/A.mi:A -pkg-sources moonbitlang/core/B:./B -target wasm-gc -workspace-path .
+            moonc build-package -w -a -o ./target/wasm-gc/release/bundle/prelude/prelude.core -pkg moonbitlang/core/prelude -pkg-sources moonbitlang/core/prelude:./prelude -target wasm-gc -workspace-path .
+            moonc build-package ./Orphan/lib.mbt -w -a -o ./target/wasm-gc/release/bundle/Orphan/Orphan.core -pkg moonbitlang/core/Orphan -pkg-sources moonbitlang/core/Orphan:./Orphan -target wasm-gc -workspace-path .
+            moonc build-package ./A/lib.mbt -w -a -o ./target/wasm-gc/release/bundle/A/A.core -pkg moonbitlang/core/A -pkg-sources moonbitlang/core/A:./A -target wasm-gc -workspace-path .
+            moonc build-package ./C/lib.mbt -w -a -o ./target/wasm-gc/release/bundle/C/C.core -pkg moonbitlang/core/C -i ./target/wasm-gc/release/bundle/A/A.mi:A -pkg-sources moonbitlang/core/C:./C -target wasm-gc -workspace-path .
+            moonc build-package ./B/lib.mbt -w -a -o ./target/wasm-gc/release/bundle/B/B.core -pkg moonbitlang/core/B -i ./target/wasm-gc/release/bundle/A/A.mi:A -pkg-sources moonbitlang/core/B:./B -target wasm-gc -workspace-path .
             moonc bundle-core ./target/wasm-gc/release/bundle/A/A.core ./target/wasm-gc/release/bundle/B/B.core ./target/wasm-gc/release/bundle/C/C.core ./target/wasm-gc/release/bundle/Orphan/Orphan.core ./target/wasm-gc/release/bundle/prelude/prelude.core -o ./target/wasm-gc/release/bundle/core.core
         "#]],
     );
