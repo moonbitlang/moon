2829c55acca430b5aa224af8b7df84a0ebbc04d7
Guest0x0 <guest0x0@outlook.com>
2025-12-10 03:05:22 +0000

    update test output

--- crates/moon/tests/test_cases/mod.rs
+++ crates/moon/tests/test_cases/mod.rs
@@ -1011,7 +1011,7 @@ fn moon_test_with_failure_json() {
         &output,
         // should keep in this format, it's used in ide test explorer
         expect![[r#"
-            {"package":"username/hello/lib1","filename":"hello.mbt","index":"0","test_name":"test_1","message":"$ROOT/src/lib1/hello.mbt:7:3-7:24 FAILED: test_1 failed"}
+            {"package":"username/hello/lib1","filename":"hello.mbt","index":"0","test_name":"test_1","message":"@username/hello/lib1:hello.mbt:7:3-7:24 FAILED: test_1 failed"}
             Total tests: 2, passed: 1, failed: 1.
         "#]],
     );
