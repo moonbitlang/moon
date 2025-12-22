0b5e3a001308edad5cb9fdad36fc595afc46efd1
Yu Zhang <yu.zhang.yz862@yale.edu>
2025-12-22 15:57:19 +0800

    chore: fmt

--- crates/moon/tests/test_cases/dedup_diag/mod.rs
+++ crates/moon/tests/test_cases/dedup_diag/mod.rs
@@ -5,8 +5,11 @@ fn test_dedup_diag() {
     let dir = TestDir::new("dedup_diag.in");
     let out = get_stdout(&dir, ["test", "--output-json"]);
 
-    check(out, expect![[r#"
+    check(
+        out,
+        expect![[r#"
         {"$message_type":"diagnostic","level":"warning","loc":{"path":"$ROOT/test.mbt","start":{"line":3,"col":7},"end":{"line":3,"col":8}},"message":"Warning (unused_value): Unused variable 'a'","error_code":2}
         Total tests: 1, passed: 1, failed: 0.
-    "#]])
+    "#]],
+    )
 }
