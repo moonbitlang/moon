bfe27427bdbd345d3a08d8f3af2b3c89630393f3
Guest0x0 <guest0x0@outlook.com>
2026-01-06 10:55:37 +0800

    simplify test driver

--- crates/moon/tests/test_cases/mod.rs
+++ crates/moon/tests/test_cases/mod.rs
@@ -2923,7 +2923,7 @@ fn moon_test_target_js_panic_with_sourcemap() {
             [username/hello] test lib/hello_test.mbt:1 ("hello") failed: Error
                 at $panic ($ROOT/_build/js/debug/test/lib/lib.blackbox_test.js:3:9)
                 at username$hello$lib_blackbox_test$$__test_68656c6c6f5f746573742e6d6274_0 ($ROOT/src/lib/hello_test.mbt:3:5)
-                at username$hello$lib_blackbox_test$$moonbit_test_driver_internal_js_catch ($ROOT/src/lib/__generated_driver_for_blackbox_test.mbt:479:11)
+                at username$hello$lib_blackbox_test$$moonbit_test_driver_internal_js_catch ($ROOT/src/lib/__generated_driver_for_blackbox_test.mbt:414:11)
             Total tests: 1, passed: 0, failed: 1."#]],
     );
 }
