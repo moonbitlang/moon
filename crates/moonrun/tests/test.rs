5c5fb1b0c5aaae58295f558b859135613f1feae0
Yu Zhang <yu.zhang.yz862@yale.edu>
2026-01-10 16:04:29 +0800

    update test

--- crates/moonrun/tests/test.rs
+++ crates/moonrun/tests/test.rs
@@ -173,9 +173,9 @@ fn test_moonrun_wasm_stack_trace() {
         &normalized_s,
         expect![[r#"
             RuntimeError: unreachable
-                at wasm://wasm:wasm-function[23]
-                at wasm://wasm:wasm-function[21]
-                at wasm://wasm:wasm-function[28]
+                at wasm://wasm:wasm-function[26]
+                at wasm://wasm:wasm-function[24]
+                at wasm://wasm:wasm-function[31]
         "#]],
     );
 
